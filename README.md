87afcb3a7572c0fdf7726be346c7c9b7a3b357e2
Jefferson-ux <jeffersonestebanniquenmaqui@gmail.com>
2025-09-07 14:13:21 -0500

    Modificando archivos clonados desde el directorio local

--- README.md
+++ README.md
@@ -1,5 +1,11 @@
 # Nuevo-Repositorio
+
 Primer repositorio creado para entender Github
 
 ## Descripción
+
 Este es un ejemplo de repositorio en GITHUB creado para entender el uso de GITHUB y la creación de repositorios
+****
+<h3 style="color:blue">Imagen de Bubble Sort</h3>
+
+![Image](https://programacionpython80889555.wordpress.com/wp-content/uploads/2023/01/bs2.png)
--- ordenamiento.py
+++ ordenamiento.py
@@ -4,14 +4,16 @@ my_list = [4,2,6,9,10,1,3,8,5,7]
 
 def bubble_sort(parameter_list):
     """ Funcion de ordenamiento burbuja"""
-    for i,elem in enumerate(parameter_list):
-        actual = elem
-        post = parameter_list[i+1] if i<len(parameter_list)-1 else actual
-        temp = actual
-        if actual > post:
-            parameter_list[i]=post
-            parameter_list[i+1]=temp
-        time.sleep(.1)
+    n = len(parameter_list)
+    for i in range(1,n):
+        for j in range (0,n-1):
+            if parameter_list[j]>parameter_list[j+1]:
+                temp = parameter_list[j]
+                parameter_list[j] = parameter_list[j+1]
+                parameter_list[j+1]= temp
+            time.sleep(.1)
+    return parameter_list
 
 bubble_sort(my_list)
 print(my_list)
+
